a26f13fffddaaa1e284aa90e81184a80876fffe3
Alexander Dymo <alex@alexdymo.com>
2012-07-27 17:33:03 +0300

    Initial emacs configuration: does nothing, just adds current dir (~/.emacs.d) to the load-path

---
+++ init.el
@@ -0,0 +1,6 @@
+;;; init.el --- emacs settings entry point.
+;;
+;; Copyright (c) 2012 Alexander Dymo
+
+;; add current dir to emacs load path
+(add-to-list 'load-path (file-name-directory load-file-name) "emacs.d dir")
