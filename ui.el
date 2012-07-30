93e4599be9d0d3d8e446ce1b8f034582b8f25936
Alexander Dymo <alex@alexdymo.com>
2012-07-30 19:21:19 +0300

    Set emacs frame size: width 100 chars and maximum height

--- ui.el
+++ ui.el
@@ -9,4 +9,12 @@
 ;; set font to Courier New, 9pt
 (set-face-attribute 'default nil :height 90)
 
+;; set window size:
+;; width: 100 chars
+(add-to-list 'default-frame-alist (cons 'width 100))
+;; height: fit to screen height
+(add-to-list 'default-frame-alist
+    (cons 'height
+        (/ (- (x-display-pixel-height) 50) (frame-char-height))))
+
 (provide 'ui)
