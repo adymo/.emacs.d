db5cf54d17e5fafab42b38f96018cd5550dd0dd8
Alexander Dymo <alex@alexdymo.com>
2012-07-31 09:07:13 +0300

    Modern pgup/pgdown that move point to the beginning/end of the buffer when there's nothing to scroll

--- editor.el
+++ editor.el
@@ -6,5 +6,18 @@
 (setq scroll-step 1
     scroll-conservatively 10000
     scroll-margin 5)
+;; Page down/up move the point to the beginning or end of the buffer
+(defun my-scroll-up ()
+    (interactive)
+    (condition-case nil (scroll-up)
+      (end-of-buffer (goto-char (point-max)))))
+(defun my-scroll-down ()
+    (interactive)
+    (condition-case nil (scroll-down)
+      (beginning-of-buffer (goto-char (point-min)))))
+(global-set-key [next] 'my-scroll-up)
+(global-set-key (kbd "C-v") 'my-scroll-up)
+(global-set-key [prior] 'my-scroll-down)
+(global-set-key (kbd "M-v") 'my-scroll-down)
 
 (provide 'editor)
